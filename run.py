c4d5bdda7939a2ebfa2783a443c463f96f20f912
Culan O'Meara <culanomeara@gmail.com>
2022-11-19 17:42:51 +0000

    Print chosen player name from random list

--- run.py
+++ run.py
@@ -7,6 +7,8 @@ import os
 import random
 import time
 
+players = []
+game_player = ""
 
 class Player:
     def __init__(self, name, defence, attack, stamina, skill, injury, luck):
@@ -29,30 +31,30 @@ p6 = Player("Benzema", 0, 0, 0, 0, 0, 0)
 p7 = Player("Lewandowski", 0, 0, 0, 0, 0, 0)
 p8 = Player("Neymar", 0, 0, 0, 0, 0, 0)
 p9 = Player("Obafemi", 0, 0, 0, 0, 0, 0)
-p10 = Player("O Meara", 0, 0, 0, 0, 0, 0)
+p10 = Player("Kerr", 0, 0, 0, 0, 0, 0)
 
 
 def intro():
     """
     Tell the player what the game is about.
     """
-    print("Welcome to Football Stars")
-    print("The game that puts you 90 minutes away from glory")
+    print("                     Welcome to Football Stars                   ")
+    print("         The game that puts you 90 minutes away from glory       ")
     print("You select your player. The player has randomly assigned attributes")
-    print("Start the match and choose actions as the game plays out")
-    print("The outcome depends on your player attributes")
+    print("     Start the match and choose actions as the game plays out")
+    print("         The outcome depends on your player attributes")
 
     ready_game = input("Are you ready?")
-    print(f"You have said {ready_game}")
     display_players()
-    game_player = validate_input()
-    print(f"You have chosen {game_player}")
+    
 
 
 def display_players():
     """
     Display 5 random players for user to choose from
     """
+    
+
     (
         first_player,
         second_player,
@@ -74,27 +76,38 @@ def display_players():
         },
         5,
     )
+
+    players = [first_player, second_player, third_player, fourth_player, fifth_player]
     print(f"Player 1: {first_player}")
     print(f"Player 2: {second_player}")
     print(f"Player 3: {third_player}")
     print(f"Player 4: {fourth_player}")
     print(f"Player 5: {fifth_player}")
+    
+    game_player_num = validate_input(1, 5)
+    global game_player
+    game_player = players[game_player_num - 1]
+    print(f"You have chosen {game_player}")
+    print("The player stats are " )
 
 
-def validate_input():
+def validate_input(int1, int2):
     """
     Validates input and gives error message if invalid
     """
+    low = int1
+    high = int2
+
     while True:
         try:
-            player_choice = int(input("Please make your choice: "))
-            assert 0 < player_choice < 6
+            user_choice = int(input("Please make your choice: "))
+            assert low <= user_choice <= high
         except ValueError:
             print("Not a number! Please enter a number.")
         except AssertionError:
-            print("Please enter a number between 1 and 5")
+            print(f"Please enter a number between {low} and {high}")
         else:
-            return player_choice
+            return user_choice
         
         
 
